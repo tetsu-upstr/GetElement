2347e06fb4f65fdecf192c36c99541157779c18b
tetsu-upstr <bodomu@gmail.com>
2020-10-25 18:12:37 +0900

    add and Search

--- Form/main.js
+++ Form/main.js
@@ -4,17 +4,22 @@ const formItemList = document.querySelectorAll(".item");
 const formLabelList = document.querySelectorAll("label");
 
 // クラス名を指定。ドット[ .]はつけない HTMLCollection 
-const ItemList = document.getElementsByClassName("item"); 
+const ItemList = document.getElementsByClassName("item email"); 
 for(let i = 0; i < ItemList.length; i++) {
     // 同じクラスの全ての入力値を取得
     console.log(ItemList.item(i).value);
+
 }
 
+// 両方のクラスを持つ要素を指定
+// document.getElementsByClassName("item email")
+
+
 // HTMLCollectionを配列に変換
-const ItemArray = Array.prototype.slice.call(ItemList);
-ItemArray.forEach(function(array) {
-    console.log(array);
-});
+// const ItemArray = Array.prototype.slice.call(ItemList);
+// ItemArray.forEach(function(array) {
+//     console.log(array);
+// });
 
 
 // console.log(formLabelList[1]);
